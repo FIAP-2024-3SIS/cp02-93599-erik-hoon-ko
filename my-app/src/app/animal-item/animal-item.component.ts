6b7d49098c90c9b766164a65d9849ecb8af296f9
Erik <logonrmlocal@fiap.com.br>
2024-04-24 22:38:25 -0300

    feat: criação do arquivo Angular

---
+++ my-app/src/app/animal-item/animal-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Animal } from '../models/animals';
+
+@Component({
+  selector: 'app-animal-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal-item.component.html',
+  styleUrl: './animal-item.component.css'
+})
+export class AnimalItemComponent {
+
+  @Input() animal: Animal = new Animal("", "", 0, "");
+
+}
